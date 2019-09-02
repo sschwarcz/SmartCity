e7408afa46f9cea7c557bc6c6c4dfe1bb1a00694
SamSch <37340452+sschwarcz@users.noreply.github.com>
2019-09-02 15:16:36 +0300

    Update EnterCam.py
    EntryCam:
    this code active the camera and let her run in the back process ,
    when people appears in the camera it create a tread that will distinguish every person and to each one it will:
    -crop the face from the frame
    -resize it
    -and save it into the directory of all the people that entered
    the code have been tested and avoid a lot of bug

--- EnterCam.py
+++ EnterCam.py
@@ -1,9 +1,6 @@
-# active la camera ,
-#repere le visage
-#prends une photo
-#rogne la photo
-from builtins import print
+#Samuel Schwarcz
 
+from builtins import print
 from termcolor import colored #not important
 import cv2
 import matplotlib.pyplot as plt
@@ -21,27 +18,27 @@ import imutils
 from imutils import face_utils
 import argparse
 
+
 predictor_path = r'C:\Users\Dell\Desktop\projetFinal\Models\shape_predictor_68_face_landmarks2.dat'
 
-
-
 database_directory='C:\\Users\\Dell\\Desktop\\DirPers\\person'
+#directory for the trash
 test_directory=r'C:\Users\Dell\Desktop\DirExit'
 
-class Index():
+class Index():#indexes are used to save or move picture 
     indexImg = 0
     indexDir = 1
     indexPhotog = 0
     img_size = 64
     NbFaces = 0
-
+#this inexes are used in case of not readeability of the picture the program will take new datas
     cam=None
     faces=None
     frame = None
 
 
-
-class Camera(Thread):####################################CAMERA
+#step 3 the camera has detected people and crated a new thread that will crop resize and save every people on the frame
+class Camera(Thread):
 
     def __init__(self,cam,faces,frame):
         Thread.__init__(self)
@@ -50,11 +47,12 @@ class Camera(Thread):####################################CAMERA
         self.cam=cam
         self.n_img_per_person=1
 
-
+    # move the picture to the trash directory
     def moveImage(self,path):
         os.rename('C:\\Users\\Dell\\Desktop\\DirPers\\'+path,test_directory+'\\'+path)
         pass
 
+    # get the new data picture from the camera runing in the back that update these data
     def getNewPicture(self):
         self.frame =Index.frame
         self.cam=Index.cam
@@ -62,7 +60,9 @@ class Camera(Thread):####################################CAMERA
         pass
 
 
-
+    #check in the directory if there are already such of a person using a model that will encode the picture 
+    #to compare it withe the other picture from the directory, if there is a problem 
+    #with the encoding it will move the picture to the trash 
     def isDouble(self,picture):
         image_dir_basepath = 'C:\\Users\\Dell\\Desktop\\DirPers\\'
         list = os.listdir(image_dir_basepath)
@@ -100,7 +100,7 @@ class Camera(Thread):####################################CAMERA
                     return True
 
         return False
-
+#step 4: croping and resizing every faces
     def PrintPicture(self, photo, cam,x ,y ,w ,h):
 
         frame = cam.read()
@@ -116,7 +116,7 @@ class Camera(Thread):####################################CAMERA
             self.getNewPicture();
             self.run()
             return
-        already = self.isDouble(aligned)
+        already = self.isDouble(aligned)  # check the doubles
 
 
         if(already==True):
@@ -149,15 +149,15 @@ class Camera(Thread):####################################CAMERA
 
         print("2-Camera found new face!")
         frame = self.frame
-
+         #every faces will be worked independently
         for face in self.faces:
             photo = PhotoIndexes()
             i = 0
 
-            while not i == self.n_img_per_person:  # 10 pictures have been taken
+            while not i == self.n_img_per_person:  # in case i want to take more than one picture
                 (x, y, w, h) = face_utils.rect_to_bb(face)
                 cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
-
+                #fonction that will crop and resize
                 self.PrintPicture(photo, self.cam, x - 20, y-80, w+40, h+120 )
                 i += 1 #number of picture for each man (=1)
 
@@ -166,10 +166,10 @@ class Camera(Thread):####################################CAMERA
 
 
         print("sorti")
+##end CAMERA
 
 
-
-
+#class for saving picture data
 class PhotoIndexes():
     synchronyzed = 1;
 
@@ -181,13 +181,11 @@ class PhotoIndexes():
         Index.indexPhotog+=1
         self.pathDir = ""
         self.nameImgDIR = ""
+## end PhotoIndex
 
 
-
-
-
-
-class FaceDemo(object):###############la camera dans le vide
+#step 2 the camera works on the back and wait for people 
+class FaceDemo(object):###############the camera works 
     def __init__(self, cascade_path):
         self.vc = None
         self.predictor = cascade_path
@@ -202,8 +200,7 @@ class FaceDemo(object):###############la camera dans le vide
         self.is_interrupted = True
 
     def capture_images(self , name='Unknown'):
-        cam = VideoStream(0).start()#########################################################################################################
-        ###############################################################################################################################
+        cam = VideoStream(0).start()
         self.vc=cam
 
 
@@ -216,26 +213,30 @@ class FaceDemo(object):###############la camera dans le vide
         detector = dlib.get_frontal_face_detector()#dlib
         predictor = dlib.shape_predictor(predictor_path)
 
-
-
+ 
+        #the camera continue to work in the back 
+        #when it detect faces it creates a new thread that will take the frame it will crop it on the face resize it
+        #check if the picture taken is clean to work with and save it into a directory with all the people that entered
         while True:
 
             frame = cam.read()
             # frame = imutils.resize(frame, width=400)
+            #save the camera in an index case of not readeability of the face or the frame
             Index.cam=cam
             gray = frame
             gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)##give a normal color to the camera
              # faces = detector(gray, 0)
             faces = detector(gray, 1)
-
+            #save the face from detector in case of not readeability of the face or the frame
             Index.faces = faces
 
             if len(faces)!=0:
-
+                   #save the frame in case of not readeability of the face or the frame
                 Index.frame=frame
                 for face in faces:
 
-                     # explain face predictor
+                     # this part is completly optional , is it for circlying people when the frame appear 
+                     #on the screen of the computer but will not influence the all course of the script
                     shape = predictor(gray, face)
                     shape = face_utils.shape_to_np(shape)
                     for (x, y) in shape:##shape.length=68
@@ -243,22 +244,24 @@ class FaceDemo(object):###############la camera dans le vide
 
                     # (x, y, w, h) = face_utils.rect_to_bb(face)
                     # cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
-
+                    
+                    
+                #check if the number of face have increased (that will mean that there is a other person
+                #that appeared on the camera so we have to create a new thread that will save it)
                 if len(faces) > Index.NbFaces:
-
 
                     print('1-create new thread')
 
                     thread1 = Camera(cam,faces,frame)
                     thread1.start()
 
-
+             ##optional for the screen presentation
             cv2.imshow("Frame",frame)
             # plt.title("Found {0} faces!".format(len(faces)))
             # plt.xticks([])
             # plt.yticks([])
             # display.clear_output(wait=True)
-
+            
             Index.NbFaces = len(faces)
 
             if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -267,8 +270,10 @@ class FaceDemo(object):###############la camera dans le vide
 
         cv2.destroyAllWindows()
         cam.stop()
+##end FaceDemo
 
 
+#step 1 start the script
 print(colored("start",'blue'))
 f = FaceDemo(predictor_path)
 f.capture_images('ENTER')
